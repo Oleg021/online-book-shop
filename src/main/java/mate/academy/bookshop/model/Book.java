7537d6a188c0a1d9cfd49e0ec48234c4cb54be8a
kankeii <vyrvyjo@gmail.com>
2025-02-20 17:53:03 +0200

    added book logics

---
+++ src/main/java/mate/academy/bookshop/model/Book.java
@@ -0,0 +1,29 @@
+package mate.academy.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Data
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @NotNull
+    @Column(unique = true)
+    private String isbn;
+    @NotNull
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
---
+++ src/main/java/mate/academy/bookshop/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.bookshop.repository.impl;
+
+import java.util.List;
+import mate.academy.bookshop.model.Book;
+import mate.academy.bookshop.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Could not save book to db" + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> getAllBookQuery = session.createQuery("FROM Book", Book.class);
+            return getAllBookQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not find all books", e);
+        }
+    }
+}
