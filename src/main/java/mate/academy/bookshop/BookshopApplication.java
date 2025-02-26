f0f809692518c010d95fb8d16a3c4629470fe4a0
kankeii <vyrvyjo@gmail.com>
2025-02-26 12:58:43 +0200

    added (BookDto + Mapper + EntityNotFoundException)

--- src/main/java/mate/academy/bookshop/BookshopApplication.java
+++ src/main/java/mate/academy/bookshop/BookshopApplication.java
@@ -1,35 +1,12 @@
 package mate.academy.bookshop;
 
-import java.math.BigDecimal;
-import mate.academy.bookshop.model.Book;
-import mate.academy.bookshop.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookshopApplication {
-    @Autowired
-    private BookService bookService;
-
     public static void main(String[] args) {
         SpringApplication.run(BookshopApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book = new Book();
-            book.setPrice(BigDecimal.valueOf(500));
-            book.setTitle("Atlas Shrugged");
-            book.setAuthor("Ayn Rand");
-            book.setDescription("Very nice book !!!");
-            book.setCoverImage("https://static.yakaboo.ua/media/catalog/product/4/9/497069_69650593.jpg");
-            book.setIsbn("9780525934189");
-            bookService.save(book);
-            bookService.findAll();
-        };
-    }
 }
--- src/main/java/mate/academy/bookshop/service/BookService.java
+++ src/main/java/mate/academy/bookshop/service/BookService.java
@@ -1,10 +1,13 @@
 package mate.academy.bookshop.service;
 
 import java.util.List;
-import mate.academy.bookshop.model.Book;
+import mate.academy.bookshop.dto.BookDto;
+import mate.academy.bookshop.dto.CreateBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto bookRequestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findById(Long id);
 }
