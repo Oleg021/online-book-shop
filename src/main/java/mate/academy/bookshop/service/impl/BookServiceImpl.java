ba341351cd8a458203713e25bc9da47c8a502d4f
kankeii <vyrvyjo@gmail.com>
2025-03-11 17:02:23 +0200

    added (Liquibase, JPARepository, deleteById and update methods)

--- src/main/java/mate/academy/bookshop/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/impl/BookServiceImpl.java
@@ -37,4 +37,22 @@ public class BookServiceImpl implements BookService {
         );
         return bookMapper.toDto(book);
     }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(Long id, CreateBookRequestDto bookRequestDto) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id " + id));
+        book.setTitle(bookRequestDto.getAuthor());
+        book.setAuthor(bookRequestDto.getAuthor());
+        book.setIsbn(bookRequestDto.getIsbn());
+        book.setPrice(bookRequestDto.getPrice());
+        book.setDescription(bookRequestDto.getDescription());
+        book.setCoverImage(bookRequestDto.getCoverImage());
+        book = bookRepository.save(book);
+    }
 }
