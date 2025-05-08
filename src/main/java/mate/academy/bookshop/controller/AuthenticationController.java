4a8eed65c95aa82800759e6a33b57f36514453cb
kankeii <vyrvyjo@gmail.com>
2025-05-08 15:17:03 +0300

    added jwt token

--- src/main/java/mate/academy/bookshop/controller/AuthenticationController.java
+++ src/main/java/mate/academy/bookshop/controller/AuthenticationController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookshop.dto.user.UserLoginRequestDto;
+import mate.academy.bookshop.dto.user.UserLoginResponseDto;
 import mate.academy.bookshop.dto.user.UserRegistrationRequestDto;
 import mate.academy.bookshop.dto.user.UserResponseDto;
 import mate.academy.bookshop.exceptions.RegistrationException;
+import mate.academy.bookshop.security.AuthenticationService;
 import mate.academy.bookshop.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "Register new user",
             description = "Registers a new user and returns user details.")
@@ -26,5 +30,12 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request.getEmail(), request);
+    }
+
+    @Operation(summary = "Login user",
+            description = "Authenticates a user and returns JWT token")
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
